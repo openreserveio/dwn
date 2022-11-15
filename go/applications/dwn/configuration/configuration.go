b09085e3d19c7a8d08ed19244b9cd9cbee1d8876
Michael Dowling <mzero@openreserve.io>
2022-11-14 16:12:04 -0800

    skeleton of web and gin for #1

---
+++ go/applications/dwn/configuration/configuration.go
@@ -0,0 +1,45 @@
+package configuration
+
+import (
+	"github.com/openreserveio/dwn/go/applications/dwn/configuration/fileenv"
+	"github.com/openreserveio/dwn/go/log"
+	"os"
+)
+
+const (
+	CONFIG_TYPE_FILEENV = "fileenv"
+	CONFIG_TYPE_ETCD    = "etcd"
+)
+
+type Configuration interface {
+
+	// API Configuration
+	GetAPIListenAddress() string
+	GetAPIListenPort() int
+}
+
+func Config() (Configuration, error) {
+
+	configTypeFlag := os.Getenv("OR_CONFIG")
+
+	var config Configuration
+	switch configTypeFlag {
+
+	case CONFIG_TYPE_FILEENV:
+		log.Info("Configuration Type:  File/Environment")
+		config = fileenv.CreateFileEnvironmentConfiguration()
+
+	case CONFIG_TYPE_ETCD:
+		log.Info("Configuration Type:  Distributed/ETCD")
+		log.Fatal("Not yet supported")
+		os.Exit(1)
+
+	default:
+		log.Info("Configuration Type:  File/Environment")
+		config = fileenv.CreateFileEnvironmentConfiguration()
+
+	}
+
+	return config, nil
+
+}
