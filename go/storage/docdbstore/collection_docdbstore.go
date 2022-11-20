de87a76fa4d77abfef1af2853c281adca2b82414
Michael Dowling <mzero@openreserve.io>
2022-11-19 16:07:46 -0800

    mongodb storage, requirements and service design docs

--- go/storage/docdbstore/collection_docdbstore.go
+++ go/storage/docdbstore/collection_docdbstore.go
@@ -1,6 +1,30 @@
 package docdbstore
 
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 type CollectionDocumentDBStore struct {
+	Client *mongo.Client
+	DB     *mongo.Database
+}
+
+func CreateCollectionDocumentDBStore(connectionUri string) (*CollectionDocumentDBStore, error) {
+
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(connectionUri))
+	if err != nil {
+		return nil, err
+	}
+
+	dbStore := CollectionDocumentDBStore{
+		Client: client,
+		DB:     client.Database("dwn_collections"),
+	}
+
+	return &dbStore, nil
+
 }
 
 func (store *CollectionDocumentDBStore) GetSchemaURI() string {
---
+++ req_arch_design/README.md
@@ -0,0 +1,10 @@
+# Requirements, Architecture, and Design
+
+## Purpose
+
+This area of the repository is for requirements proposals/finalization, architecture standards, and design proposals/finalizations.
+
+## Organization
+
+TBD.  
+
