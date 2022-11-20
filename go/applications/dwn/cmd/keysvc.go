40d81604b4c56799a243f95eb7ee934641549ea2
Michael Dowling <mzero@openreserve.io>
2022-11-20 13:38:44 -0800

    Key service for message signing and verification

---
+++ go/applications/dwn/cmd/keysvc.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"github.com/openreserveio/dwn/go/applications/dwn/configuration"
+	"github.com/openreserveio/dwn/go/applications/dwn/service/keysvc"
+	"github.com/openreserveio/dwn/go/log"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+// keysvcCmd represents the keysvc command
+var keysvcCmd = &cobra.Command{
+	Use:   "keysvc",
+	Short: "OpenReserve DWN Backend Key Service",
+	Long:  `Backend gRPC Service for managing keys, authenticating messages, and signing messages`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		log.Info("Starting DWN Backend Key Service")
+		config, err := configuration.Config()
+		if err != nil {
+			log.Fatal("Configuration Fatal Error:  %v", err)
+			os.Exit(1)
+		}
+		log.Error("Stopping Key Service:  %v", keysvc.Start(config))
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(keysvcCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// orgapiCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// orgapiCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
