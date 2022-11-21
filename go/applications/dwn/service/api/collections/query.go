f602a2d1ec903b1a74cffda00b0ccc05eb59dfc1
Michael Dowling <mzero@openreserve.io>
2022-11-21 11:04:34 -0800

    CollectionsQuery at the API level #8

--- go/applications/dwn/service/api/collections/query.go
+++ go/applications/dwn/service/api/collections/query.go
@@ -1,9 +1,78 @@
 package collections
 
-import "github.com/openreserveio/dwn/go/model"
+import (
+	"context"
+	"fmt"
+	"github.com/openreserveio/dwn/go/generated/services"
+	"github.com/openreserveio/dwn/go/model"
+	"net/http"
+)
 
-func CollectionsQuery() model.MessageResultObject {
+func CollectionsQuery(collSvcClient services.CollectionServiceClient, message *model.Message) model.MessageResultObject {
 
-	return model.MessageResultObject{}
+	var messageResultObj model.MessageResultObject
+
+	// First, make sure authorizations are valid and correct for this message
+	if !model.VerifyAuthorization(message) {
+		messageResultObj.Status = model.ResponseStatus{Code: http.StatusBadRequest, Detail: "Unable to verify authorization(s)."}
+		return messageResultObj
+	}
+
+	// Next, find the schema and make sure it has been registered
+	schemaUri := message.Descriptor.Schema
+	if schemaUri == "" {
+		messageResultObj.Status = model.ResponseStatus{Code: http.StatusBadRequest, Detail: "Schema URI is required for a CollectionsWrite"}
+		return messageResultObj
+	}
+
+	// Is this for a specific record, or all records since context?
+	if message.RecordID == "" {
+		// TODO:  For now we are only going to allow single message access
+		messageResultObj.Status = model.ResponseStatus{Code: http.StatusBadRequest, Detail: "TODO:  For now we are only going to allow single message access"}
+		return messageResultObj
+	}
+
+	// Get the collection item
+	req := services.FindCollectionRequest{
+		QueryType:        services.QueryType_SINGLE_COLLECTION_BY_ID_SCHEMA_URI,
+		CollectionItemId: message.RecordID,
+		SchemaURI:        message.Descriptor.Schema,
+	}
+
+	findCollResponse, err := collSvcClient.FindCollection(context.Background(), &req)
+	if err != nil {
+		messageResultObj.Status = model.ResponseStatus{Code: http.StatusInternalServerError, Detail: err.Error()}
+		return messageResultObj
+	}
+
+	switch findCollResponse.Status.Status {
+
+	case services.Status_OK:
+		messageResultEntry := model.MessageResultEntry{
+			Result: findCollResponse.CollectionItem,
+		}
+		messageResultObj.Entries = append(messageResultObj.Entries, messageResultEntry)
+		messageResultObj.Status = model.ResponseStatus{Code: http.StatusOK}
+		return messageResultObj
+
+	case services.Status_NOT_FOUND:
+		messageResultObj.Status = model.ResponseStatus{Code: http.StatusNotFound, Detail: fmt.Sprintf("Unable to locate collection item with record ID %s", message.RecordID)}
+		return messageResultObj
+
+	case services.Status_INVALID_AUTHORIZATION:
+		messageResultObj.Status = model.ResponseStatus{Code: http.StatusUnauthorized}
+		return messageResultObj
+
+	case services.Status_ERROR:
+		messageResultObj.Status = model.ResponseStatus{Code: http.StatusInternalServerError, Detail: findCollResponse.Status.Details}
+		return messageResultObj
+
+	default:
+		messageResultObj.Status = model.ResponseStatus{Code: http.StatusInternalServerError, Detail: "Default Error"}
+		return messageResultObj
+
+	}
+
+	return messageResultObj
 
 }
--- go/model/util.go
+++ go/model/util.go
@@ -241,3 +241,98 @@ func CreateAttestation(message *Message, privateKey ecdsa.PrivateKey) DWNJWS {
 	return attestation
 
 }
+
+func VerifyAuthorization(message *Message) bool {
+
+	encodedProtectedHeader := message.Authorization.Signatures[0].Protected
+	encodedSignature := message.Authorization.Signatures[0].Signature
+	encodedPayload := message.Authorization.Payload
+
+	// Make sure the payloads match as expected
+	expectedPayload := map[string]string{
+		"descriptorCid": CreateDescriptorCID(message.Descriptor),
+		"processingCid": CreateProcessingCID(message.Processing),
+	}
+	expectedJsonPayload, _ := json.Marshal(&expectedPayload)
+	expectedJwsPayload := base64.URLEncoding.EncodeToString(expectedJsonPayload)
+
+	if expectedJwsPayload != encodedPayload {
+		// These should match
+		return false
+	}
+
+	// Get the ecdsa.PublicKey
+	jsonProtectedHeader, err := base64.URLEncoding.DecodeString(encodedProtectedHeader)
+	if err != nil {
+		return false
+	}
+
+	var protectedHeaderMap map[string]string
+	err = json.Unmarshal(jsonProtectedHeader, &protectedHeaderMap)
+	if err != nil {
+		return false
+	}
+
+	attestorDid := protectedHeaderMap["kid"]
+	if attestorDid == "" {
+		return false
+	}
+
+	res := did.ResolvePublicKey(attestorDid)
+	if res == nil {
+		return false
+	}
+
+	var publicKey *ecdsa.PublicKey = res.(*ecdsa.PublicKey)
+	err = jwt.SigningMethodES512.Verify(fmt.Sprintf("%s.%s", encodedProtectedHeader, encodedPayload), encodedSignature, publicKey)
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+func CreateAuthorization(message *Message, privateKey ecdsa.PrivateKey) DWNJWS {
+
+	authorization := DWNJWS{}
+
+	// PEM encode public key -> multibase(base64url)
+	publicKey := privateKey.PublicKey
+	publicKeyBytes, _ := x509.MarshalPKIXPublicKey(&publicKey)
+	pemEncodedPublicKey := pem.EncodeToMemory(&pem.Block{Type: "PUBLIC EC KEY", Bytes: publicKeyBytes})
+	publicKeyMultibase, _ := multibase.Encode(multibase.Base64url, pemEncodedPublicKey)
+
+	protectedHeader := map[string]string{
+		"typ": "JWS",
+		"alg": jwt.SigningMethodES512.Alg(),
+		"kid": fmt.Sprintf("did:key:%s", publicKeyMultibase),
+	}
+	jsonProtectedHeader, _ := json.Marshal(&protectedHeader)
+	jwsProtectedHeader := base64.URLEncoding.EncodeToString(jsonProtectedHeader)
+
+	payload := map[string]string{
+		"descriptorCid": CreateDescriptorCID(message.Descriptor),
+		"processingCid": CreateProcessingCID(message.Processing),
+	}
+	jsonPayload, _ := json.Marshal(&payload)
+	jwsPayload := base64.URLEncoding.EncodeToString(jsonPayload)
+
+	var jwsPayloadBytes []byte = make([]byte, base64.URLEncoding.EncodedLen(len(jsonPayload)))
+	base64.URLEncoding.Encode(jwsPayloadBytes, jsonPayload)
+
+	sig, err := jwt.SigningMethodES512.Sign(fmt.Sprintf("%s.%s", jwsProtectedHeader, jwsPayload), &privateKey)
+	if err != nil {
+
+	}
+
+	authorization.Payload = jwsPayload
+	authorization.Signatures = []DWNJWSSig{
+		{
+			Protected: jwsProtectedHeader,
+			Signature: sig,
+		},
+	}
+
+	return authorization
+
+}
