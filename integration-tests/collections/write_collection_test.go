7a31b84981e4fa868753f3e9302e66ec589fb3f2
Michael Dowling <mzero@openreserve.io>
2022-11-21 09:45:20 -0800

    Successful collectionswrite integration test

--- integration-tests/collections/write_collection_test.go
+++ integration-tests/collections/write_collection_test.go
@@ -4,8 +4,13 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"encoding/json"
+	"github.com/go-resty/resty/v2"
 	. "github.com/onsi/ginkgo/v2"
-	"github.com/openreserveio/dwn/go/did"
+	. "github.com/onsi/gomega"
+	"github.com/openreserveio/dwn/go/model"
+	"github.com/openreserveio/dwn/integration-tests/testutils"
+	"net/http"
 )
 
 var _ = Describe("Write Collection", func() {
@@ -14,11 +19,42 @@ var _ = Describe("Write Collection", func() {
 
 		authorPrivateKey, _ := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
 		authorPublicKey := authorPrivateKey.PublicKey
-		authorDID := did.CreateKeyDID(&authorPublicKey)
+		authorDID, _ := testutils.CreateKeyDID(&authorPublicKey)
 
 		recipientPrivateKey, _ := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
 		recipientPublicKey := recipientPrivateKey.PublicKey
-		recipientDID := did.CreateKeyDID(&recipientPublicKey)
+		recipientDID, _ := testutils.CreateKeyDID(&recipientPublicKey)
+
+		body := []byte("{\"name\":\"test\"}")
+
+		It("Stores the message correctly", func() {
+
+			message := testutils.CreateMessage(authorDID, recipientDID, "application/json", body, "CollectionsWrite", "")
+			attestation := testutils.CreateAttestation(message, *authorPrivateKey)
+			message.Attestation = attestation
+			message.Descriptor.Schema = "https://openreserve.io/schemas/test.json"
+
+			ro := model.RequestObject{}
+			ro.Messages = append(ro.Messages, *message)
+
+			res, err := resty.New().R().
+				SetBody(ro).
+				SetHeader("Content-Type", "application/json").
+				Post("http://localhost:8080/")
+
+			Expect(err).To(BeNil())
+			Expect(res).ToNot(BeNil())
+			Expect(res.StatusCode()).To(Equal(http.StatusOK))
+
+			var responseObject model.ResponseObject
+			err = json.Unmarshal(res.Body(), &responseObject)
+			Expect(err).To(BeNil())
+
+			Expect(responseObject.Status.Code).To(Equal(http.StatusOK))
+			Expect(len(responseObject.Replies)).To(Equal(1))
+			Expect(responseObject.Replies[0].Status.Code).To(Equal(http.StatusOK))
+
+		})
 
 	})
 
--- integration-tests/go.mod
+++ integration-tests/go.mod
@@ -5,12 +5,32 @@ go 1.19
 require (
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-resty/resty/v2 v2.7.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/ipfs/go-cid v0.3.2 // indirect
+	github.com/ipld/go-ipld-prime v0.19.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/minio/sha256-simd v1.0.0 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/multiformats/go-base32 v0.1.0 // indirect
+	github.com/multiformats/go-base36 v0.2.0 // indirect
+	github.com/multiformats/go-multibase v0.1.1 // indirect
+	github.com/multiformats/go-multicodec v0.7.0 // indirect
+	github.com/multiformats/go-multihash v0.2.1 // indirect
+	github.com/multiformats/go-varint v0.0.6 // indirect
 	github.com/onsi/ginkgo/v2 v2.5.1 // indirect
 	github.com/onsi/gomega v1.24.1 // indirect
 	github.com/openreserveio/dwn/go v0.0.0-20221121160453-ac71bb17e859 // indirect
+	github.com/polydawn/refmt v0.0.0-20201211092308-30ac6d18308e // indirect
+	github.com/rs/zerolog v1.28.0 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	golang.org/x/crypto v0.3.0 // indirect
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	lukechampine.com/blake3 v1.1.7 // indirect
 )
