379158e30274f4ba0ac0d3e10a6584c239e88800
Michael Dowling <mzero@openreserve.io>
2022-11-22 09:43:02 -0800

    message chain structure; latest entry and latest checkpoints

--- go/storage/collection_store.go
+++ go/storage/collection_store.go
@@ -1,6 +1,9 @@
 package storage
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
 
 // Base interface for stores for collections
 // Will probably need:
@@ -20,16 +23,27 @@ type CollectionItem struct {
 }
 
 type CollectionRecord struct {
-	ID         primitive.ObjectID `bson:"_id"`
-	RecordID   string             `bson:"record_id"`
-	CreatorDID string             `bson:"creator_did"`
-	OwnerDID   string             `bson:"owner_did"`
-	WriterDIDs []string           `bson:"writer_dids"`
-	ReaderDIDs []string           `bson:"reader_dids"`
+	ID                      primitive.ObjectID `bson:"_id"`
+	RecordID                string             `bson:"record_id"`
+	CreatorDID              string             `bson:"creator_did"`
+	OwnerDID                string             `bson:"owner_did"`
+	WriterDIDs              []string           `bson:"writer_dids"`
+	ReaderDIDs              []string           `bson:"reader_dids"`
+	InitialEntryID          string             `bson:"initial_entry_id"`
+	LatestEntryID           string             `bson:"latest_entry_id"`
+	LatestCheckpointEntryID string             `bson:"latest_checkpoint_entry_id"`
 }
 
 type MessageEntry struct {
-	ID       primitive.ObjectID `bson:"_id"`
-	RecordID string             `bson:"record_id"`
-	Schema   string             `bson:"schema"`
+	ID             primitive.ObjectID `bson:"_id"`
+	MessageEntryID string             `bson:"message_entry_id"`
+	ParentEntryID  string             `bson:"parent_message_entry_id"`
+	RecordID       string             `bson:"record_id"`
+	Schema         string             `bson:"schema"`
+	Method         string             `bson:"method"`
+	Data           []byte             `bson:"data"`
+	DataCID        string             `bson:"dataCID"`
+	Protocol       string             `bson:"protocol"`
+	CreatedDate    time.Time          `bson:"created_date"`
+	PublishedDate  time.Time          `bson:"published_date"`
 }
