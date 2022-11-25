4a1392282ec55ea3faaa2e9c0d58e575da344759
Michael Dowling <mzero@openreserve.io>
2022-11-25 08:49:10 -0800

    finishing up data access layer for collection records and message entry

--- go/storage/docdbstore/collection_docdbstore.go
+++ go/storage/docdbstore/collection_docdbstore.go
@@ -2,7 +2,9 @@ package docdbstore
 
 import (
 	"context"
+	"errors"
 	"github.com/google/uuid"
+	"github.com/openreserveio/dwn/go/log"
 	"github.com/openreserveio/dwn/go/storage"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -84,16 +86,57 @@ func (store *CollectionDocumentDBStore) CreateCollectionRecord(record *storage.C
 }
 
 func (store *CollectionDocumentDBStore) AddCollectionMessageEntry(entry *storage.MessageEntry) error {
-	//TODO implement me
-	panic("implement me")
+
+	// Get Record
+	if entry.RecordID == "" {
+		return errors.New("No Record ID")
+	}
+	collectionRecord := store.GetCollectionRecord(entry.RecordID)
+	if collectionRecord == nil {
+		return errors.New("No Record Found")
+	}
+
+	_, err := store.DB.Collection("dwn_collections").InsertOne(context.Background(), entry)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
 }
 
 func (store *CollectionDocumentDBStore) GetMessageEntryByID(messageEntryID string) *storage.MessageEntry {
-	//TODO implement me
-	panic("implement me")
+
+	res := store.DB.Collection("dwn_collections").FindOne(context.Background(), bson.D{{"message_entry_id", messageEntryID}})
+	if res.Err() != nil {
+		log.Error("Error getting message entry by ID:  %v", res.Err())
+		return nil
+	}
+
+	var messageEntry storage.MessageEntry
+	err := res.Decode(&messageEntry)
+	if err != nil {
+		log.Error("Error decoding result of getting message entry by ID:  %v", err)
+		return nil
+	}
+
+	return &messageEntry
 }
 
 func (store *CollectionDocumentDBStore) GetCollectionRecord(recordId string) *storage.CollectionRecord {
-	//TODO implement me
-	panic("implement me")
+
+	res := store.DB.Collection("dwn_collections").FindOne(context.Background(), bson.D{{"record_id", recordId}})
+	if res.Err() != nil {
+		log.Error("Error getting record by ID:  %v", res.Err())
+		return nil
+	}
+
+	var collectionRecord storage.CollectionRecord
+	err := res.Decode(&collectionRecord)
+	if err != nil {
+		log.Error("Error decoding result of getting collectionRecord by ID:  %v", err)
+		return nil
+	}
+
+	return &collectionRecord
 }
