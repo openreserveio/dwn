3c2ae790a6f538374c091c85a198e4635b135cf5
Michael Dowling <mzero@openreserve.io>
2022-12-19 13:36:35 -0800

    Remove collection item

--- go/storage/docdbstore/collection_docdbstore.go
+++ go/storage/docdbstore/collection_docdbstore.go
@@ -7,7 +7,6 @@ import (
 	"github.com/openreserveio/dwn/go/log"
 	"github.com/openreserveio/dwn/go/storage"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -30,36 +29,6 @@ func CreateCollectionDocumentDBStore(connectionUri string) (*CollectionDocumentD
 	}
 
 	return &dbStore, nil
-
-}
-
-func (store *CollectionDocumentDBStore) GetCollectionItem(identifier string) (*storage.CollectionItem, error) {
-
-	// Get Collection
-	var collectionItem storage.CollectionItem
-	result := store.DB.Collection("dwn_collections").FindOne(context.Background(), bson.D{{"_id", identifier}})
-	err := result.Decode(&collectionItem)
-	if err != nil {
-		return nil, err
-	}
-
-	return &collectionItem, nil
-
-}
-
-func (store *CollectionDocumentDBStore) PutCollectionItem(collectionItem *storage.CollectionItem) error {
-
-	if collectionItem.ID.IsZero() {
-		// this is a new item
-		collectionItem.ID = primitive.NewObjectID()
-	}
-
-	_, err := store.DB.Collection("dwn_collections").InsertOne(context.Background(), collectionItem)
-	if err != nil {
-		return err
-	}
-
-	return nil
 
 }
 
