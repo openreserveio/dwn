19cda3357d17e54f963403fe9438de29264ca6bd
Michael Dowling <mzero@openreserve.io>
2022-12-29 10:46:05 -0800

    starting impl on #25

--- go/applications/dwn/service/api/collections/commit.go
+++ go/applications/dwn/service/api/collections/commit.go
@@ -1,1 +1,16 @@
 package collections
+
+import (
+	"github.com/openreserveio/dwn/go/generated/services"
+	"github.com/openreserveio/dwn/go/model"
+	"net/http"
+)
+
+func CollectionsCommit(collSvcClient services.CollectionServiceClient, message *model.Message) model.MessageResultObject {
+
+	return model.MessageResultObject{
+		Status:  model.ResponseStatus{Code: http.StatusOK},
+		Entries: nil,
+	}
+
+}
