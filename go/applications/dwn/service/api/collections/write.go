01eff9d5e1671b4502cdabf75a0f5011900eda2f
Michael Dowling <mzero@openreserve.io>
2022-12-29 09:58:30 -0800

    logic changed for #35, updated find collection response

--- go/applications/dwn/service/api/collections/write.go
+++ go/applications/dwn/service/api/collections/write.go
@@ -13,6 +13,7 @@ func CollectionsWrite(collSvcClient services.CollectionServiceClient, message *m
 	var messageResultObj model.MessageResultObject
 
 	// First, make sure attestations are valid and correct for this message
+	// TODO:  Deal with whitelisting, blacklisting, authentication requirements
 	if !model.VerifyAttestation(message) {
 		messageResultObj.Status = model.ResponseStatus{Code: http.StatusUnauthorized, Detail: "Unable to verify attestation(s)."}
 		return messageResultObj
@@ -20,15 +21,40 @@ func CollectionsWrite(collSvcClient services.CollectionServiceClient, message *m
 
 	// Make sure authorizations are valid for messages that are writes to existing records
 	// Check for existing record
-	findCollResp, err := collSvcClient.FindCollection(context.Background(), &services.FindCollectionRequest{RecordId: message.RecordID})
+	findCollResp, err := collSvcClient.FindCollection(context.Background(), &services.FindCollectionRequest{QueryType: services.QueryType_SINGLE_COLLECTION_BY_ID_SCHEMA_URI, SchemaURI: message.Descriptor.Schema, RecordId: message.RecordID})
 	if err != nil {
 		messageResultObj.Status = model.ResponseStatus{Code: http.StatusInternalServerError, Detail: err.Error()}
 		return messageResultObj
 	}
-	
-	if !model.VerifyAuthorization(message) {
-		messageResultObj.Status = model.ResponseStatus{Code: http.StatusUnauthorized, Detail: "Unable to verify authorization(s)."}
-		return messageResultObj
+
+	// If no record was found, then we don't need to authorize
+	var foundCollMessage model.Message
+	json.Unmarshal(findCollResp.CollectionItem, &foundCollMessage)
+
+	switch findCollResp.Status.Status {
+
+	case services.Status_NOT_FOUND:
+	// No need to authorize!
+
+	case services.Status_OK:
+		// We found a record.  Must authorize
+		if !model.VerifyAuthorization(message) {
+			messageResultObj.Status = model.ResponseStatus{Code: http.StatusUnauthorized, Detail: "Unable to verify authorization(s)."}
+			return messageResultObj
+		}
+
+		authorized := false
+		for _, v := range findCollResp.Writers {
+			if v == message.Processing.AuthorDID {
+				authorized = true
+			}
+		}
+
+		if !authorized {
+			messageResultObj.Status = model.ResponseStatus{Code: http.StatusUnauthorized, Detail: "Author is not authorized to write to this record."}
+			return messageResultObj
+		}
+
 	}
 
 	// Next, find the schema and make sure it has been registered
