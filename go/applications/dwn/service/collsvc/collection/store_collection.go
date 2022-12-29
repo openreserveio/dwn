24b7abbbdcc7faf6b4669e3d08922dcd2ddc6278
Michael Dowling <mzero@openreserve.io>
2022-12-29 11:46:19 -0800

    implemented commit logic in collection service for #25

--- go/applications/dwn/service/collsvc/collection/store_collection.go
+++ go/applications/dwn/service/collsvc/collection/store_collection.go
@@ -2,14 +2,20 @@ package collection
 
 import (
 	"errors"
-	"github.com/openreserveio/dwn/go/log"
 	"github.com/openreserveio/dwn/go/model"
 	"github.com/openreserveio/dwn/go/storage"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 const (
-	ERR_DUPLICATE_INITIAL_ENTRY = "Trying to write an Initial Entry to a record that already exists"
+	ERR_DUPLICATE_INITIAL_ENTRY                     = "Trying to write an Initial Entry to a record that already exists"
+	ERR_COMMIT_TO_RECORD_NOT_FOUND                  = "Trying to commit to a record that does not yet exist"
+	ERR_COMMIT_TO_RECORD_CHECKPOINT_ENTRY_NOT_FOUND = "Trying to commit to a record that does not have a latest checkpoint"
+
+	ERR_MUTATE_UMMUTABLE_VALUE     = "Attempt to mutate an immutable value"
+	ERR_MISMATCHED_COMMIT_STRATEGY = "Commit Strategy value in existing checkpoint record does not match the commitStrategy value specified in the inbound message,"
+
+	ERR_COMMIT_MESSAGE_CREATE_DATE_BEFORE_WRITE = "Commit message's created date is before the latest write message."
 )
 
 type StoreCollectionResult struct {
@@ -47,6 +53,60 @@ func StoreCollection(collectionStore storage.CollectionStore, collectionMessage 
 	return &result, nil
 }
 
+func collectionsCommit(collectionsStore storage.CollectionStore, collectionsCommitMessage *model.Message) error {
+
+	// Retrieve the currently active CollectionsWrite entry for the recordId specified in the inbound CollectionsCommit
+	// message. If there is no currently active CollectionsWrite entry, discard the inbound message and cease processing.
+	existingCollRecord := collectionsStore.GetCollectionRecord(collectionsCommitMessage.RecordID)
+	if existingCollRecord == nil {
+		return errors.New(ERR_COMMIT_TO_RECORD_NOT_FOUND)
+	}
+
+	latestCheckpointEntry := collectionsStore.GetMessageEntryByID(existingCollRecord.LatestCheckpointEntryID)
+	if latestCheckpointEntry == nil {
+		return errors.New(ERR_COMMIT_TO_RECORD_CHECKPOINT_ENTRY_NOT_FOUND)
+	}
+
+	// Ensure all immutable values from the Initial Entry remained unchanged if present in the inbound message.
+	// If any have been mutated, discard the message and cease processing.
+	// Ensure all immutable values from the Initial Entry remained unchanged if present in the
+	// inbound message. If any have been mutated, discard the message and cease processing.
+	initialMessageEntry := collectionsStore.GetMessageEntryByID(existingCollRecord.InitialEntryID)
+	if initialMessageEntry == nil {
+		return errors.New("Unable to find an initial entry")
+	}
+	if initialMessageEntry.Descriptor.Protocol != collectionsCommitMessage.Descriptor.Protocol ||
+		initialMessageEntry.Descriptor.ProtocolVersion != collectionsCommitMessage.Descriptor.ProtocolVersion ||
+		initialMessageEntry.Descriptor.Schema != collectionsCommitMessage.Descriptor.Schema {
+		return errors.New("Attempt to mutate an immutable value")
+	}
+
+	// If the currently active CollectionsWrite does not have a commitStrategy value, or the value does not
+	// match the commitStrategy value specified in the inbound message, discard the message and cease processing.
+	if latestCheckpointEntry.Descriptor.CommitStrategy != collectionsCommitMessage.Descriptor.CommitStrategy {
+		return errors.New(ERR_MISMATCHED_COMMIT_STRATEGY)
+	}
+
+	// he parentId of the message MUST match the currently active CollectionsWrite message’s Entry ID or that of
+	// another CollectionsCommit that descends from it. If the parentId does not match any of the messages in the
+	// commit tree, discard the inbound message and cease processing.
+	// This is done by way of searching for the parent ID
+
+	// The inbound message’s entry dateCreated value is less than the dateCreated value of the message in the commit
+	// tree its parentId references, discard the message and cease processing.
+	if collectionsCommitMessage.Descriptor.DateCreated.Before(latestCheckpointEntry.Descriptor.DateCreated) {
+		return errors.New(ERR_COMMIT_MESSAGE_CREATE_DATE_BEFORE_WRITE)
+	}
+
+	// If all of the above steps are successful, store the message in relation to the record.
+	// I think this means we set the latest checkpoint to the latest entry
+	existingCollRecord.LatestEntryID = existingCollRecord.LatestCheckpointEntryID
+	collectionsStore.SaveCollectionRecord(existingCollRecord)
+
+	return nil
+
+}
+
 func collectionsWrite(collectionStore storage.CollectionStore, collectionsWriteMessage *model.Message) error {
 
 	/*
@@ -60,8 +120,8 @@ func collectionsWrite(collectionStore storage.CollectionStore, collectionsWriteM
 	processingId := model.CreateProcessingCID(collectionsWriteMessage.Processing)
 	entryId := model.CreateRecordCID(descriptorId, processingId)
 
-	log.Info(" Entry ID: %s", entryId)
-	log.Info("Record ID: %s", collectionsWriteMessage.RecordID)
+	//log.Info(" Entry ID: %s", entryId)
+	//log.Info("Record ID: %s", collectionsWriteMessage.RecordID)
 
 	// TODO:  Come back to this.  This should match
 	// if entryId == collectionsWriteMessage.RecordID {
@@ -119,7 +179,7 @@ func collectionsWrite(collectionStore storage.CollectionStore, collectionsWriteM
 		if initialMessageEntry.Descriptor.Protocol != collectionsWriteMessage.Descriptor.Protocol ||
 			initialMessageEntry.Descriptor.ProtocolVersion != collectionsWriteMessage.Descriptor.ProtocolVersion ||
 			initialMessageEntry.Descriptor.Schema != collectionsWriteMessage.Descriptor.Schema {
-			return errors.New("Attempt to mutate an immutable value")
+			return errors.New(ERR_MUTATE_UMMUTABLE_VALUE)
 		}
 
 		// Retrieve the Latest Checkpoint Entry, which will be either the Initial Entry or the latest CollectionsDelete,
