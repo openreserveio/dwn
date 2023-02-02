64556e70ab804e0888b65ab1dd2132fe5641b91a
Michael Dowling <mzero@openreserve.io>
2023-02-01 18:02:56 -0800

    deployment changes to k8s and base #41

--- deployment/alpha/terraform/variables.tf
+++ deployment/alpha/terraform/variables.tf
@@ -4,3 +4,16 @@ variable "cluster_name" {
   type        = string
   default     = ""
 }
+
+variable "eks_cluster_domain" {
+  type        = string
+  description = "Route53 domain for the cluster."
+  default     = "example.com"
+}
+
+variable "acm_certificate_domain" {
+  type        = string
+  description = "Route53 certificate domain"
+  default     = "*.example.com"
+}
+
