c86ba8038d125219e9157437d86fd95bb064bcf3
Michael Dowling <mzero@openreserve.io>
2023-02-10 12:39:34 -0800

    hooks write API, hook storage

--- go/applications/dwn/service/api/hooks/write.go
+++ go/applications/dwn/service/api/hooks/write.go
@@ -3,9 +3,15 @@ package hooks
 import (
 	"context"
 	"github.com/openreserveio/dwn/go/model"
+	"github.com/openreserveio/dwn/go/observability"
 )
 
 func HooksWrite(ctx context.Context, message *model.Message) model.MessageResultObject {
 
+	// Instrumentation
+	_, childSpan := observability.Tracer.Start(ctx, "CollectionsCommit")
+	defer childSpan.End()
+
 	return model.MessageResultObject{}
+
 }
