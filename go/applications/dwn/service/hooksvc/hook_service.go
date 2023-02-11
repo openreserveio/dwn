bcd83dc3cfc69b6ef47bf39d50514ea0864c9b73
Michael Dowling <mzero@openreserve.io>
2023-02-11 12:01:58 -0800

    Hook service

--- go/applications/dwn/service/hooksvc/hook_service.go
+++ go/applications/dwn/service/hooksvc/hook_service.go
@@ -3,14 +3,30 @@ package hooksvc
 import (
 	"context"
 	"github.com/openreserveio/dwn/go/generated/services"
+	"github.com/openreserveio/dwn/go/log"
+	"github.com/openreserveio/dwn/go/storage"
+	"github.com/openreserveio/dwn/go/storage/docdbstore"
 )
 
 type HookService struct {
 	services.UnimplementedHookServiceServer
+	HookStore storage.HookStore
 }
 
-func CreateHookService() (HookService, error) {
-	return HookService{}, nil
+func CreateHookService(hookStoreConnectionURI string) (*HookService, error) {
+
+	// Setup Hook Store
+	hookStore, err := docdbstore.CreateHookDocumentDBStore(hookStoreConnectionURI)
+	if err != nil {
+		log.Fatal("Unable to connect to hook store:  %v", err)
+		return nil, err
+	}
+
+	hookService := HookService{
+		HookStore: hookStore,
+	}
+
+	return &hookService, nil
 }
 
 func (hookService HookService) RegisterHook(ctx context.Context, request *services.RegisterHookRequest) (*services.RegisterHookResponse, error) {
