62bc60b02d900d7f3379299e30face0f27a564c0
Michael Dowling <mzero@openreserve.io>
2023-02-12 07:54:12 -0800

    added reading a collection item

--- go/client/dwn_client.go
+++ go/client/dwn_client.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-resty/resty/v2"
 	"github.com/google/uuid"
 	"github.com/openreserveio/dwn/go/model"
+	"github.com/openreserveio/dwn/go/storage"
 	"net/http"
 	"time"
 )
@@ -37,6 +38,63 @@ func CreateDWNClientForProtocol(urlBase string, protocol string, protocolVersion
 		Protocol:        protocol,
 		ProtocolVersion: protocolVersion,
 	}
+
+}
+
+func (client *DWNClient) GetData(schemaUrl string, recordId string, requestorIdentity *Identity) ([]byte, string, error) {
+
+	queryDescriptor := model.Descriptor{
+		Method: model.METHOD_COLLECTIONS_QUERY,
+		Filter: model.DescriptorFilter{
+			RecordID: recordId,
+			Schema:   schemaUrl,
+		},
+	}
+
+	queryMessageProcessing := model.MessageProcessing{
+		Nonce:        uuid.NewString(),
+		AuthorDID:    requestorIdentity.DID,
+		RecipientDID: requestorIdentity.DID,
+	}
+
+	queryMessage := model.Message{
+		ContextID:  "",
+		Processing: queryMessageProcessing,
+		Descriptor: queryDescriptor,
+	}
+
+	authorization := model.CreateAuthorization(&queryMessage, *requestorIdentity.Keypair.PrivateKey)
+	attestation := model.CreateAttestation(&queryMessage, *requestorIdentity.Keypair.PrivateKey)
+	queryMessage.Attestation = attestation
+	queryMessage.Authorization = authorization
+
+	ro := model.RequestObject{}
+	ro.Messages = append(ro.Messages, queryMessage)
+
+	res, err := resty.New().R().
+		SetBody(ro).
+		SetHeader("Content-Type", "application/json").
+		Post(client.DWNUrlBase)
+
+	if err != nil {
+		return nil, "", err
+	}
+
+	var responseObject model.ResponseObject
+	err = json.Unmarshal(res.Body(), &responseObject)
+	if err != nil {
+		return nil, "", err
+	}
+
+	// TODO: Change this return object -- shouldn't be a message entry from storage package
+	var entry storage.MessageEntry
+	json.Unmarshal(responseObject.Replies[0].Entries[0].Result, &entry)
+	data, err := base64.RawURLEncoding.DecodeString(entry.Data)
+	if err != nil {
+		return nil, "", err
+	}
+
+	return data, entry.Descriptor.DataFormat, nil
 
 }
 
