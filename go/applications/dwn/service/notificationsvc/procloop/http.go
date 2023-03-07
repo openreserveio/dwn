ff228249ef86eedf886f0d2402bd1e0650e45040
Michael Dowling <mzero@openreserve.io>
2023-03-07 13:07:21 -0800

    hook service notifications for #44 #66

--- go/applications/dwn/service/notificationsvc/procloop/http.go
+++ go/applications/dwn/service/notificationsvc/procloop/http.go
@@ -1,1 +1,13 @@
 package procloop
+
+import (
+	"github.com/openreserveio/dwn/go/generated/events"
+	"github.com/openreserveio/dwn/go/log"
+)
+
+func NotifyHTTP(event *events.Event) error {
+
+	log.Info("HTTP NOTIFY EVENT:  %v", event)
+	return nil
+
+}
--- go/applications/dwn/service/notificationsvc/procloop/loop.go
+++ go/applications/dwn/service/notificationsvc/procloop/loop.go
@@ -1,8 +1,21 @@
 package procloop
 
-import "github.com/openreserveio/dwn/go/generated/events"
+import (
+	"errors"
+	"fmt"
+	"github.com/openreserveio/dwn/go/generated/events"
+)
 
 func ProcessMessage(event *events.Event) error {
+
+	switch event.EventType {
+
+	case events.EventType_NOTIFY_CALLBACK_HTTP:
+		return NotifyHTTP(event)
+
+	default:
+		return errors.New(fmt.Sprintf("Unknown Event Type:  %d", event.EventType))
+	}
 
 	return nil
 }
